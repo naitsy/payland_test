0653f9f6897948b88d78d67a98be667a6ca529e7
naitsy <flores.javier@gmail.com>
2015-05-30 15:17:11 -0300

    Commit inicial
    Commit inicial de prueba de un proyecto .net 2010.

---
+++ application/Test/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Test text", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
